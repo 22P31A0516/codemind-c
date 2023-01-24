e8659963f6fb19695648ae9beb5e2031bc92271f
22P31A0516 <121609724+22P31A0516@users.noreply.github.com>
2023-01-24 21:32:22 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     if(n==1)     {         printf("monday");     }     else if(n==2)     {         printf("tuesday");     }     else if(n==3)     {         printf("wednesday");     }     else if(n==4)     {         printf("thursday");     }     else if(n==5)     {         printf("friday");     }     else if(n==6)     {         printf("saturday");     }     else if(n==7)     {         printf("sunday");     }     else     {         printf("invalid input");     } }

--- Day_name__.c
+++ Day_name__.c
@@ -1,31 +1,35 @@
 #include<stdio.h>
 int main()
 {
-    char n;
-    scanf("%c",&n);
-    if(n == '1')
+    int n;
+    scanf("%d",&n);
+    if(n==1)
     {
         printf("monday");
     }
-    else if(n == '2')
+    else if(n==2)
     {
         printf("tuesday");
     }
-    else if(n == '3')
+    else if(n==3)
     {
         printf("wednesday");
     }
-    else if(n == '4')
+    else if(n==4)
     {
         printf("thursday");
     }
-    else if(n == '5')
+    else if(n==5)
     {
         printf("friday");
     }
-    else if(n == '6')
+    else if(n==6)
     {
         printf("saturday");
+    }
+    else if(n==7)
+    {
+        printf("sunday");
     }
     else
     {
